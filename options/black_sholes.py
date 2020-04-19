bfce34a85bd88cc79db299b4e30893630dffd89d
JacopoAlbertoGentile <jacopoalbertogentile@users.noreply.github.com>
2020-04-19 16:37:18 -0400

    uploaded code

--- options/black_sholes.py
+++ options/black_sholes.py
@@ -20,7 +20,7 @@ class BlackScholes(object):
         self.d2=None
 
 
-    def Price(self,vol):
+    def price(self,vol):
         d1 = (m.log(self.underlying / self.strike) + (self.rate + 0.5 * vol ** 2) * self.T) / (vol * m.sqrt(self.T))
         d2 = d1 - (vol) * m.sqrt(self.T)
         if self.type =='Call' or self.type =="C":
@@ -30,7 +30,7 @@ class BlackScholes(object):
             put=st.norm.cdf(-d2,0,1)*self.strike*self.discount_factor-st.norm.cdf(-d1,0,1)*self.underlying
             return put
 
-    def Delta(self,vol):
+    def delta(self,vol):
         d1 = (m.log(self.underlying / self.strike) + (self.rate + 0.5 * vol ** 2) * self.T) / (
                     vol * m.sqrt(self.T))
         d2 = d1 - (vol) * m.sqrt(self.T)
@@ -48,19 +48,19 @@ class BlackScholes(object):
         vega=self.underlying*st.norm.cdf(d1,0,1)*m.sqrt(self.T)
         return(vega)
 
-    def ImpliedVol(self,Quote,vol_est):
-        print((self.Price(vol_est),self.Delta(vol_est),self.Vega(vol_est)))
+    def implied_vol(self,Quote,vol_est):
+        print((self.price(vol_est),self.delta(vol_est),self.Vega(vol_est)))
         i=1
         while(i<=100):
-            vol_est=vol_est-((self.Price(vol_est)-Quote)/self.Vega(vol_est))
-            if abs(self.Price(vol_est)-Quote) <= 0.0001:
+            vol_est=vol_est-((self.price(vol_est)-Quote)/self.Vega(vol_est))
+            if abs(self.price(vol_est)-Quote) <= 0.0001:
                 print("Numbers of attemps:",i)
                 print("Implied_Vol:",vol_est)
                 return vol_est
             else:
                 i=i+1
 
-    def Volga(self,vol):
+    def volga(self,vol):
         Vega=self.Vega(vol)
         d1 = (m.log(self.underlying / self.strike) + (self.rate + 0.5 * vol ** 2) * self.T) / (
                     vol * m.sqrt(self.T))
@@ -69,7 +69,7 @@ class BlackScholes(object):
         return(volga)
 
 
-    def Vanna(self,vol):
+    def vanna(self,vol):
         d1 = (m.log(self.underlying / self.strike) + (self.rate + 0.5 * vol ** 2) * self.T) / (
                 vol * m.sqrt(self.T))
         dNorm=m.exp(-0.5*d1**2)*(1/2*m.pi)
@@ -81,19 +81,6 @@ class BlackScholes(object):
 
 
 
-
-
-
-
-#if __name__== "__main__":
-    #A=BlackScholes(3,5,0.02, "2019-09-18", "Put")
-    #print(A.Price(0.4136),A.Delta(0.4136),A.Vega(0.4136))
-    #print(A.ImpliedVol(0.3,0.3))
-    #print(A.Vanna(0.25))
-    #print(A.Volga(0.25))
-    #for i in range(10,100):
-        #A.underlying=i
-        #print(A.Vanna(0.25))
 
 
 
--- options/vanna_volga.py
+++ options/vanna_volga.py
@@ -1,10 +1,5 @@
-import numpy as np
-import pandas as pd
-import math
-import matplotlib.pyplot as plt
-import scipy.stats as st
-from BlackScholes import *
-import matplotlib.pyplot as plt
+from options.black_sholes import BlackScholes
+
 
 
 #set the strike and ivols in other words create the Ivol_surface
@@ -19,11 +14,11 @@ Call3=BlackScholes(300,200,0.02,"2019-12-18","Call")
 
 Bs_Iv=0.25
 
-PriceCall1=Call1.Price(Bs_Iv)
-PriceCall2=Call2.Price(Bs_Iv)
-PriceCall3=Call3.Price(Bs_Iv)
+priceCall1=Call1.price(Bs_Iv)
+priceCall2=Call2.price(Bs_Iv)
+priceCall3=Call3.price(Bs_Iv)
 
-print([PriceCall1,PriceCall2,PriceCall3])
+print([priceCall1,priceCall2,priceCall3])
 #Calculating Weights in Vanna-Volga framework
 
 weight1=(Call2.Vega(Bs_Iv)/Call1.Vega(Bs_Iv))*((math.log(b2[1]/b2[1])*math.log(b3[1]/b2[1]))/(math.log(b2[1]/b1[1])*math.log(b3[1]/b1[1])))
@@ -31,12 +26,12 @@ weight2=(Call2.Vega(Bs_Iv)/Call2.Vega(Bs_Iv))*((math.log(b2[1]/b1[1])*math.log(b
 weight3=(Call2.Vega(Bs_Iv)/Call3.Vega(Bs_Iv))*((math.log(b2[1]/b1[1])*math.log(b2[1]/b2[1]))/(math.log(b3[1]/b1[1])*math.log(b3[1]/b2[1])))
 
 print([weight1,weight2,weight3])
-print(PriceCall2)
+print(priceCall2)
 
 
 OptionsBS=BlackScholes(200,200,0.02,"2019-12-18",'Call')
-print(OptionsBS.Price(Bs_Iv))
-Option=OptionsBS.Price(Bs_Iv)+weight2*(b2[2]-PriceCall2)
+print(OptionsBS.price(Bs_Iv))
+Option=OptionsBS.price(Bs_Iv)+weight2*(b2[2]-priceCall2)
 
 
 class Vanna_Volga(BlackScholes):
@@ -45,13 +40,13 @@ class Vanna_Volga(BlackScholes):
 
 
 
-    def BSPrices(self,vol,strikes):
+    def bs_prices(self,vol,strikes):
         initial_strike=self.strike
         Options={}
         for i in strikes:
             self.strike=i
 
-            Options[i]=self.Price(vol)
+            Options[i]=self.price(vol)
         self.strike=initial_strike
         return(Options)
 
@@ -62,8 +57,8 @@ class Vanna_Volga(BlackScholes):
 
 if __name__=="__main__":
     A=Vanna_Volga(12,10,0.02,"2019-12-20",'Call')
-    print(A.BSPrices(0.25,[9,10,11]))
+    print(A.bs_prices(0.25,[9,10,11]))
     print(A.strike)
     B=BlackScholes(12,10,0.02,"2019-12-20",'Call')
-    print(B.Price(0.25))
+    print(B.price(0.25))
 
